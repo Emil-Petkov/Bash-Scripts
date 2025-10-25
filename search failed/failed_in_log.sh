b44d3229b366281e6052eb0cbb79035c12645fda
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2025-10-25 23:33:52 +0300

    Delete search failed directory

--- search failed/failed_in_log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Search for failed logins
-echo "Enter log for analize:"
-read LOGFILE
-
-grep -iE '(fail|denied|error|unsuccessful)' "$LOGFILE" | grep -iE '(login|user|auth)' "$LOGFILE" | tee /dev/tty | wc -l | awk '{print "\033[1;31m--- TOTAL FAILS: ---\033[0m [" $1 "]"}'
-
